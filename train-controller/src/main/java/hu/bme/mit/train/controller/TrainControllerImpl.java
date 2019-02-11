4ee2dc49189f517c1532f5677d4d04337e7042da
Kristof <ivanicskristof2@gmail.com>
2019-02-11 07:20:10 -0800

    duplicated filed deleted

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -42,18 +42,15 @@ public class TrainControllerImpl implements TrainController {
 			referenceSpeed = speedLimit;
 		}
 	}
-<<<<<<< HEAD
 
 
 	//branch A change
-=======
 	//this
 	//is
 	//not
 	//the
 	//same
 	//comment
->>>>>>> branch-B
 	@Override
 	public void setJoystickPosition(int joystickPosition) {
 		this.step = joystickPosition;		
